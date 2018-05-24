a8742cb4be061f100167fc0f4cfd215515532958
cfpinedao <cfpinedao@unal.edu.co>
2018-05-24 19:47:26 -0400

    Automatic commit. Done before go to queltrics

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-
-(function () {
-    document.getElementById("btn").addEventListener("click", function () {
-        alert("Hi! :)");
+$(document).ready(function(){
+    var map;
+    $("#map").ready(function(){
+        map=new google.maps.Map(document.getElementById('map'),{
+            center: {lat: 40.7306107, lng: -73.935242},
+            zoom: 10
+        });
     });
-})();
+
+
+    $.getJSON('https://data.cityofnewyork.us/api/views/xyye-rtrs/rows.json?accessType=DOWNLOAD', function(results){;
+        $.each(results, function(key,value){
+            if(key==="data"){
+                  console.log(value[0][16]);
+            }
+          
+            
+        });
+
+    });
+
+
+});
